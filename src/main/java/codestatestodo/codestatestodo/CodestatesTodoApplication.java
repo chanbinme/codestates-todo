3b89f68cf8cffa2d252e20e1f6c9f30a170d8681
jambin <gksmfcksqls@gmail.com>
2022-12-16 15:35:14 +0900

    test

---
+++ src/main/java/codestatestodo/codestatestodo/CodestatesTodoApplication.java
@@ -0,0 +1,13 @@
+package codestatestodo.codestatestodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestatesTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodestatesTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codestatestodo/codestatestodo/CodestatesTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package codestatestodo.codestatestodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestatesTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
